f7abc92688bea9b373bd5d538a9d1537cdfbf686
Daniel Fróes <danielfroes15@gmail.com>
2020-07-15 09:13:11 -0300

    dialogue font size responsive, but with bug when loaded from init menu

---
+++ Assets/FontSizeFixer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+
+public class FontSizeFixer: MonoBehaviour
+{
+    private TextMeshProUGUI textUI;
+    private void Start() {
+        textUI = GetComponent<TextMeshProUGUI>();
+        textUI.enableAutoSizing = false;
+
+        if(gameObject.tag == "Dialogue")
+        {
+            textUI.text = "";
+            print(textUI.fontSize);
+        }
+    }
+}
