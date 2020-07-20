327f625100660bd61d56d15ee8fc71f9a7639640
Daniel Fróes <danielfroes15@gmail.com>
2020-07-20 08:35:32 -0300

    dialogue font auto size fixed

--- Assets/FontSizeFixer.cs
+++ Assets/FontSizeFixer.cs
@@ -3,18 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
-
+/*
+ *  FontSizeFixer:
+ *  Responsability: Disables auto size after the calculation of the font size.
+ *                 -> It is necessary using a dummy text to the 
+ *
+ */
 public class FontSizeFixer: MonoBehaviour
 {
     private TextMeshProUGUI textUI;
     private void Start() {
-        textUI = GetComponent<TextMeshProUGUI>();
-        textUI.enableAutoSizing = false;
 
+        textUI = GetComponent<TextMeshProUGUI>();
+        //forces the calculation of the autosize  
+        textUI.ForceMeshUpdate();
+        //disable auto sizing for performance reasons a
+        textUI.enableAutoSizing = false;
+        //if the gameObject is dialogue, reset its text;
         if(gameObject.tag == "Dialogue")
         {
-            textUI.text = "";
-            print(textUI.fontSize);
+            textUI.SetText("");
         }
     }
+
 }
